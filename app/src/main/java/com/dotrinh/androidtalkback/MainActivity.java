d38a9431363ee8610b18302e6aed98c5c19fea24
Do Trinh <thanhdo.trinh@gmail.com>
2022-12-08 22:26:06 +0900

    target to specific a view

--- app/src/main/java/com/dotrinh/androidtalkback/MainActivity.java
+++ app/src/main/java/com/dotrinh/androidtalkback/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
 
 import com.dotrinh.androidtalkback.databinding.ActivityMainBinding;
 
@@ -24,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
         binding.checkClickThrough.setOnClickListener(v -> LogI("click checkClickThough"));
         binding.button2.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_NO);
+
+        //target to specific a view
+        binding.getRoot().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.textView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED);
+            }
+        }, 300);
     }
 }
